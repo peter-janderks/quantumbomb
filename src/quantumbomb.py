792952492377f916399a69eabda0147b263d658c
tsoud1 <45370392+tsoud1@users.noreply.github.com>
2018-12-17 21:21:20 +0100

    added noise model

--- src/quantumbomb.py
+++ src/quantumbomb.py
@@ -1,17 +1,31 @@
-from projectq.ops import H, Measure, X, Rx, CNOT
+from projectq.ops import H, Measure, X, Rx, CNOT, Y, Z
 from projectq import MainEngine
 from projectq.backends._sim._simulator import Simulator
 
+import random
 import numpy 
 
 def rotation_angle(number_of_rotations):
     rotation_angle = (numpy.pi) / (number_of_rotations)
+#    print(rotation_angle, "rotation angle")
     return(rotation_angle)
 
 def quantum_bomb(eng, qubit_a, qubit_b):    
     CNOT | (qubit_a, qubit_b)
     Measure | qubit_b
     return(qubit_a, qubit_b)
+
+def pauli_error(eng, qubit_a, error_rate):
+    if random.random() < error_rate:
+        rand_num = random.random()
+        if rand_num < 1/3:
+            X | qubit_a
+        elif rand_num > 2/3:
+            Y | qubit_a
+        else:
+            Z | qubit_a
+
+    return(qubit_a)
 
 def get_all_probabilities(eng,qureg):
     i = 0
@@ -21,6 +35,7 @@ def get_all_probabilities(eng,qureg):
         qubit_list = qubit_list[::-1]
         l = eng.backend.get_probability(qubit_list,qureg)
         if l != 0.0:
+#            print("probability:", l,"state:", qubit_list)
             return(qubit_list)
         i += 1     
             
@@ -28,13 +43,17 @@ def run_once():
     eng = MainEngine()
     q1 = eng.allocate_qubit()
     q2 = eng.allocate_qubit()
+    error_on = True
     number_of_iterations = 5
+    error_rate = 0.1
     angle = rotation_angle(number_of_iterations)
 
-    for num in range(0,number_of_iterations):
+    for _ in range(0,number_of_iterations):
         Rx(angle) | q1
- 
         quantum_bomb(eng,q1,q2)
+        if error_on == True:
+            pauli_error(eng,q1,error_rate)
+        
         eng.flush()
 
     state = get_all_probabilities(eng, q1)
@@ -46,7 +65,7 @@ def state_count(iterations):
     state_count_0 = 0
     state_count_1 = 1
 
-    for i in range(0,iterations):
+    for _ in range(0,iterations):
         x = run_once()
         if x == [0]:
             state_count_0 += 1
@@ -56,4 +75,5 @@ def state_count(iterations):
     print(state_count_0, 'times 0 was measured')
 
 if __name__ == '__main__':
-    state_count(5000)
+
+    state_count(500)
