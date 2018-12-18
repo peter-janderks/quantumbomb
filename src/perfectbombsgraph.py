e0cf620a04ab521a5531748227e3cf6d3d9ed1a9
Peter-Jan <peter-jan.derks@student.auc.nl>
2018-12-18 12:33:49 +0100

    first simulation graph

--- src/perfectbombsgraph.py
+++ src/perfectbombsgraph.py
@@ -2,9 +2,15 @@ from quantumbomb import state_probability
 import matplotlib.pyplot as plt 
 
 if __name__ == '__main__':
-    for n in range(1,100):
-        state_probability(100,n)
-
+    x_axis = []
+    y_axis = []
+    for n in range(1,25,5):
+        y_axis.append(state_probability(100,n))
+        x_axis.append(n)
+    plt.plot(x_axis, y_axis)    
+    plt.ylabel('probability measuring 0')
+    plt.xlabel('number of rotation gates')
+    plt.show()
 #    plt.plot([1,2,3,4])
 #    plt.ylabel('some numbers')
 #    plt.show()
