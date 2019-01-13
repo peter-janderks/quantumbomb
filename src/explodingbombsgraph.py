38240dd7de77784b63c8bfb63f3f27aff82912f7
tsoud1 <45370392+tsoud1@users.noreply.github.com>
2019-01-13 18:37:25 +0100

    added scripts for error graph

---
+++ src/explodingbombsgraph.py
@@ -0,0 +1,27 @@
+#from quantumbomb import state_probability
+from explodingquantumbomb import succes_probability
+from matplotlib import pyplot as plt 
+import numpy as np 
+
+if __name__ == '__main__':
+    error_range = [0.01, 0.05, 0.1, 0.2 , 0.3]
+    xdata_list = np.zeros((5,25))
+    ydata_list = np.zeros((5,25))
+    for i in range(len(error_range)):
+        x_axis = []
+        y_axis = []
+        for n in range(1,26,1):
+            y_axis.append(succes_probability(100,error_range[i],n))
+            x_axis.append(n)
+
+        xdata_list[i,:] = x_axis
+        ydata_list[i,:] = y_axis
+
+    for i in range(len(error_range)):
+        plt.plot(xdata_list[i,:], ydata_list[i,:])  
+
+    plt.ylabel('probability measuring 0')
+    plt.xlabel('number of rotation gates')
+    plt.legend(error_range)
+    plt.show()
+
---
+++ src/quantumbomb2.py
@@ -0,0 +1,83 @@
+from projectq.ops import H, Measure, X, Rx, CNOT, Y, Z
+from projectq import MainEngine
+from projectq.backends._sim._simulator import Simulator
+
+import random
+import numpy 
+
+def rotation_angle(number_of_rotations):
+    rotation_angle = (numpy.pi) / (number_of_rotations)
+    return(rotation_angle)
+
+def quantum_bomb(eng, qubit_a, qubit_b):    
+    CNOT | (qubit_a, qubit_b)
+    Measure | qubit_b
+    
+    return(qubit_a, qubit_b)
+
+def pauli_error(eng, qubit_a, error_rate):
+    if random.random() < error_rate:
+        X | qubit_a
+        #rand_num = random.random()
+        #if rand_num < 1/3:
+        #    X | qubit_a
+        #elif rand_num > 2/3:
+        #    Y | qubit_a
+        #else:
+        #    Z | qubit_a
+
+    return(qubit_a)
+
+def get_all_probabilities(eng,qureg):
+    i = 0
+    y = len(qureg)
+    while i < (2**y):
+        qubit_list = [int(x) for x in list(('{0:0b}'.format(i)).zfill(y))]
+        qubit_list = qubit_list[::-1]
+        l = eng.backend.get_probability(qubit_list,qureg)
+        if l != 0.0:
+            return(qubit_list)
+        i += 1     
+            
+def run_once_without_bombs_exploding(number_of_iterations, error = False):
+    eng = MainEngine()
+    q1 = eng.allocate_qubit()
+    q2 = eng.allocate_qubit()
+    error_on = error
+    error_rate = 0.01
+    angle = rotation_angle(number_of_iterations)
+
+    for _ in range(0,number_of_iterations):
+        Rx(angle) | q1
+        quantum_bomb(eng,q1,q2)
+        if error_on == True:
+            pauli_error(eng,q1,error_rate)
+        
+        eng.flush()
+
+    state = get_all_probabilities(eng, q1)
+    Measure | q1
+    Measure | q2
+    return(state)
+
+def state_count(iterations_of_circuit, error, rotation_iterations = 5):
+    state_count_0 = 0
+    state_count_1 = 1
+
+    for _ in range(0,iterations_of_circuit):
+        x = run_once_with_bombs_exploding(rotation_iterations, error)
+        if x == [0]:
+            state_count_0 += 1
+        else:
+            state_count_1 += 1
+    return(state_count_0)        
+    print(state_count_0, 'times 0 was measured')
+
+def state_probability(iterations_of_circuit, error, rotation_iterations = 5):
+    state_count_0 = state_count(iterations_of_circuit, error, rotation_iterations)
+    print(state_count_0/iterations_of_circuit, 'times 0 was measured')
+    return(state_count_0/iterations_of_circuit)
+
+
+if __name__ == '__main__':
+    state_count(500)
